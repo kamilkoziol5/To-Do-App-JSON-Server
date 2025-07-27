0456b1cde66dfd51bd2833446307f7fa23f5029b
Kamil <kamilkoziol529@gmail.com>
2025-07-27 13:19:07 +0200

    add tempalte

--- src/JS-MODULES/_renderTaskElement.js
+++ src/JS-MODULES/_renderTaskElement.js
@@ -3,72 +3,16 @@ import { apiDeleteTask } from './_api';
 const ul = document.querySelector('.task-list');
 
 export function getTaskHTML(dataElement, editMode = false) {
-	const { date, title, body } = dataElement;
-
+	let html = '';
 	if (editMode) {
-		return `
-            <div class="task-inside">
-                <div class="task-header">
-                    <label>
-                        <span>Podaj datę</span>
-                        <input type="date" class="task-date" value="${date}">
-                    </label>
-
-                    <div class="task-actions">
-                        <button class="task-delete" title="Usuń zadanie">
-                            <i class='bx  bx-trash'  ></i> 
-                        </button>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <label>
-                        <span>Tytuł</span>
-                        <input type="text" class="task-title" value="${title}">
-                    </label>
-                </div>
-
-                <div class="row">
-                    <label>
-                        <span>Treść</span>
-                        <textarea class="task-body">${body}</textarea>
-                    </label>
-                </div>
-
-                <div class="task-footer">
-                    <button class="button task-edit-save">Zapisz</button>
-                    <button class="button task-edit-cancel button-secondary">Anuluj</button>
-                </div>
-            </div>
-        `;
+		html = document.querySelector('#taskEditTemplate').innerHTML;
 	} else {
-		return `
-            <div class="task-inside">
-                <div class="task-header">
-                    <h3 class="task-date">${date}</h3>
-
-                    <div class="task-actions">
-                        <button class="task-edit" title="Edytuj zadanie">
-                            <i class='bx  bx-edit'  ></i> 
-                        </button>
-                        <button class="task-delete" title="Usuń zadanie">
-                            <i class='bx  bx-trash'  ></i> 
-                        </button>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="task-title">${title}</div>
-                </div>
-
-                <div class="row">
-                    <div class="task-body">
-                        ${body}
-                    </div>
-                </div>
-            </div>
-        `;
+		html = document.querySelector('#taskTemplate').innerHTML;
 	}
+	html = html.replaceAll('{{date}}', dataElement.date);
+	html = html.replaceAll('{{title}}', dataElement.title);
+	html = html.replaceAll('{{body}}', dataElement.body);
+	return html;
 }
 
 export function renderSingleTask(dataElement, editMode) {
